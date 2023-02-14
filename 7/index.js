da7af7c27a53e2f2af35bda131bf2bfd7252725c
Valery <11w-walerik@mail.ru>
2023-02-14 17:43:34 +0300

    7 done

---
+++ 7/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = "Vasya";
+const isBirthday = true;
+let age;
+age = 31;
+if (isBirthday) {
+    console.log(`Congrats ${userName}, age: ${age + 1}`);
+}
---
+++ 7/index.ts
@@ -0,0 +1,9 @@
+let userName: string = "Vasya";
+const isBirthday: boolean = true;
+let age: number;
+
+age = 31;
+
+if (isBirthday) {
+    console.log(`Congrats ${userName}, age: ${age +1}`);
+}
