9b5a11f8ba902bc23d25cd7e0839f0ff2ee29575
Valery <11w-walerik@mail.ru>
2023-03-01 16:18:50 +0300

    30 done

--- 30/index.ts
+++ 30/index.ts
@@ -1,19 +1,28 @@
 interface User {
-	login: string;
+	readonly login: string;		// только для чтения
 	password: string;
 	age?: number;				// необязательное свойство
 	addr: string | undefined;	// поле обязательно, но может быть undefined
-	parents?: {
+	readonly parents?: {
 		mother?: string;
 		father?: string;
-	}
+	};
 }
 
 const user: User = {
 	login: 'Vasya',
 	password: 'QAZwsx',
-	addr: undefined
+	addr: undefined,
+};
+
+const userFreeze: Readonly<User> = {	// объект как User, но все свойства только для чтения
+	login: 'Vasya',
+	password: 'QAZwsx',
+	addr: undefined,
 }
+
+user.login = 'login2';
+user.password = 'newPass';
 
 
 let dbName: string;
@@ -28,3 +37,17 @@ function sendUserData(obj: User, db?: string): void {		// аргумент db н
 	console.log(obj.parents!.father?.toLowerCase(), db?.toLowerCase());
 }
 
+
+class Animal {
+	readonly name: string = 'name';		// только для чтения
+}
+
+
+//const basicPorts: readonly number[] = [3000, 3001, 5000];
+const basicPorts: ReadonlyArray<number> = [3000, 3001, 5000];
+basicPorts[0] = 5;
+basicPorts.push(566);
+
+// const basicPorts1: readonly [number, ...string[]] = [3000, '3001', '5000'];
+// basicPorts1[0] = 5;
+// basicPorts1.push(566);
