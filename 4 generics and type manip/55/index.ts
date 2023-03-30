4ca279e2f996cb66a032ecd7824877577481bb89
Valery <11w-walerik@mail.ru>
2023-03-30 11:10:17 +0300

    55 done

--- 4 generics and type manip/55/index.ts
+++ 4 generics and type manip/55/index.ts
@@ -1,7 +1,27 @@
+
 interface ICompany {
     name: string;
     debts: number;
+    departments: Department;
+    management: {
+        owner: string;
+    }
 }
+
+interface Department {
+    [key: string]: string;
+}
+
+
+const debts = 'debts';
+type CompanyDebtsType = ICompany[typeof debts];
+
+// type CompanyDebtsType = ICompany['debts'];                  // получение типа свойства интерфейса
+type CompanyOwnerType = ICompany['management']['owner'];
+type CompanyDeptsType = ICompany['departments'];
+type CompanyDeptsTypes = ICompany['departments'][number];   // number - число для доступа к элементу массива
+type Test = ICompany[keyof ICompany];                       // юнион тип всех свойств объекта
+
 
 type CompanyKeys = keyof ICompany;  // содержит ключи объекта
 const keys: CompanyKeys = 'name';
@@ -14,19 +34,19 @@ function printDebts<T, K extends keyof T, S extends keyof T>(
     console.log(`Company ${company[name]}, debts: ${company[debts]}`);
 }
 
-const hh: ICompany = {
+const google: ICompany = {
     name: 'HH',
-    debts: 50000
+    debts: 50000,
+    departments: {
+        sales: 'sales',
+        developer: 'dev'
+    },
+    management: {
+        owner: 'JohanPohan',
+    }
 }
 
-printDebts(hh, 'name', 'debts');
-
-const google = {
-    name: 'Google',
-    open: 'true'
-}
-
-printDebts(google, 'name', 'open');
+printDebts(google, 'name', 'debts');
 
 
 type GoogleKeys = keyof typeof google;
